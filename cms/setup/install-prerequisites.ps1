6a6b30f6094dfa2a99edf6b93462294c054524ae
acvasniuc <andrei.a.cvasniuc@gmail.com>
2021-01-20 19:11:15 +0200

    create all necessary docker structure by copying the entire docker folder

--- cms/setup/install-prerequisites.ps1
+++ cms/setup/install-prerequisites.ps1
@@ -1,27 +1,7 @@
-$rootPath = "$(Get-Item ../../../)\docker"
-$dataPath = "$rootPath\data"
-$deployPath = "$rootPath\deploy"
-$traefikPath = "$rootPath\traefik"
-
-Write-Host "Creating required directories"
-New-Item -ItemType directory -Force -Path $rootPath
-New-Item -ItemType directory -Force -Path $dataPath
-New-Item -ItemType directory -Force -Path "$dataPath\cm"
-New-Item -ItemType directory -Force -Path "$dataPath\sql"
-New-Item -ItemType directory -Force -Path "$dataPath\solr"
-New-Item -ItemType directory -Force -Path $deployPath
-New-Item -ItemType directory -Force -Path "$deployPath\website"
-New-Item -ItemType directory -Force -Path "$deployPath\xconnect"
-New-Item -ItemType directory -Force -Path $traefikPath
-New-Item -ItemType directory -Force -Path "$traefikPath\certs"
-New-Item -ItemType directory -Force -Path "$traefikPath\config"
-New-Item -ItemType directory -Force -Path "$traefikPath\config\dynamic"
+$rootPath = "$(Get-Item ../../../)"
 
 Write-Host "Copy directories"
-Copy-Item -Path .\docker\build -Recurse -Destination $rootPath
-
-Write-Host "Copy files"
-Copy-Item -Path .\docker\clean.ps1 -Destination $rootPath
+Copy-Item -Path .\docker -Recurse -Destination $rootPath
 
 if ((Get-Command "docker" -errorAction SilentlyContinue) -eq $null)
 {
